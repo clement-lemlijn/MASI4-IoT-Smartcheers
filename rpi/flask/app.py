90cf8a98657b9a3d5f9e3addc87972239ac368e8
LEMLIJN Clément <clement.lemlijn@gmail.com>
2026-08-06 11:43:27 +0200

    Initialize Flask app with greeting route

--- rpi/flask/app.py
+++ rpi/flask/app.py
@@ -1,1 +1,16 @@
 
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/greeting", methods=["GET"])
+def greeting():
+    return "Hello World"
+
+
+if __name__ == "__main__":
+    app.run(
+        host="0.0.0.0",
+        port=5000
+    )
