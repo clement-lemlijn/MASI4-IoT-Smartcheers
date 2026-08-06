0bf54fdeb167b6e868dde247cb37a117f76612ff
LEMLIJN Clément <clement.lemlijn@gmail.com>
2026-08-06 12:13:48 +0200

    Implement camera streaming with PiCamera

--- rpi/flask/test/camera-api/app.py
+++ rpi/flask/test/camera-api/app.py
@@ -1,11 +1,15 @@
-from flask import Flask, jsonify
+from flask import Flask, Response
+from picamera import PiCamera
+from picamera.array import PiRGBArray
 import json
+import time
+from PIL import Image
+from io import BytesIO
 
 
 app = Flask(__name__)
 
 
-# Chargement config
 with open("config.json") as f:
     config = json.load(f)
 
@@ -13,29 +17,66 @@ with open("config.json") as f:
 VISIT_ID = config["visitId"]
 
 
-
-@app.route("/health", methods=["GET"])
-def health():
-
-    return jsonify({
-        "status": "online"
-    })
+camera = PiCamera()
+camera.resolution = (640, 480)
+camera.framerate = 24
 
 
+time.sleep(2)
 
-@app.route("/camera/<visit_id>", methods=["GET"])
-def camera(visit_id):
+
+def generate_frames():
+
+    raw_capture = PiRGBArray(
+        camera,
+        size=(640, 480)
+    )
+
+    for frame in camera.capture_continuous(
+        raw_capture,
+        format="bgr",
+        use_video_port=True
+    ):
+
+        image = frame.array
+
+        buffer = BytesIO()
+
+        img = Image.fromarray(
+            image
+        )
+
+        img.save(
+            buffer,
+            format="JPEG"
+        )
+
+        jpeg = buffer.getvalue()
+
+
+        yield (
+            b"--frame\r\n"
+            b"Content-Type: image/jpeg\r\n\r\n"
+            + jpeg
+            + b"\r\n"
+        )
+
+
+        raw_capture.truncate(0)
+
+
+@app.route("/camera/<visit_id>")
+def stream(visit_id):
 
     if visit_id != VISIT_ID:
-        return jsonify({
-            "error": "invalid visit id"
-        }), 403
+        return "Unauthorized", 403
 
 
-    return jsonify({
-        "message": "camera access granted",
-        "visitId": visit_id
-    })
+    return Response(
+        generate_frames(),
+        mimetype=
+        "multipart/x-mixed-replace; boundary=frame"
+    )
 
 
 
