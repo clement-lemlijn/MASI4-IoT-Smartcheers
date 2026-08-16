9812df463af8889020824e88bbad2e640c5d6cfc
LEMLIJN Clément <clement.lemlijn@gmail.com>
2026-08-16 14:15:19 +0200

    Update create-room.py

--- vm1-artemis/create-room.py
+++ vm1-artemis/create-room.py
@@ -1,8 +1,15 @@
-import requests, os
+import requests
+import os
+
+with open(".env") as f:
+    for line in f:
+        line = line.strip()
+        if line and not line.startswith("#"):
+            key, value = line.split("=", 1)
+            os.environ[key] = value
 
 TOKEN = os.environ["WEBEX_BOT_TOKEN"]
 HEADERS = {"Authorization": f"Bearer {TOKEN}"}
-
 def create_room_for_bar(nom_bar, support_emails):
     r = requests.post("https://webexapis.com/v1/rooms",
         headers=HEADERS, json={"title": f"Smartcheers - {nom_bar}"})
